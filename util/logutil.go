b3fd1bcfd60adae6b4f1a6bdcd7e813ce45a0829
aiziyuer <ziyu0123456789@gmail.com>
2021-04-18 03:17:54 -0400

    新增json格式日志

--- util/logutil.go
+++ util/logutil.go
@@ -1,11 +1,12 @@
 package util
 
 import (
+	"os"
+	"time"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
-	"os"
-	"time"
 )
 
 // SetupLogs adds hooks to send logs to different destinations depending on level
@@ -33,6 +34,30 @@ func SetupLogs(logFileName string) {
 		}), zap.LevelEnablerFunc(func(level zapcore.Level) bool {
 			return level >= zapcore.DebugLevel
 		})),
+
+		zapcore.NewCore(zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			MessageKey:  "msg",
+			LevelKey:    "level",
+			TimeKey:     "ts",
+			CallerKey:   "file",
+			EncodeLevel: zapcore.CapitalLevelEncoder,
+			EncodeTime: func(time time.Time, encoder zapcore.PrimitiveArrayEncoder) {
+				encoder.AppendString(time.Format("2006-01-02 15:04:05"))
+			},
+			EncodeDuration: func(duration time.Duration, encoder zapcore.PrimitiveArrayEncoder) {
+				encoder.AppendInt64(int64(duration) / 1000000)
+			},
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		}), zapcore.AddSync(&lumberjack.Logger{
+			Filename:   logFileName + ".json", // 日志路径
+			MaxSize:    5,                     // 日志大小, 单位是M
+			MaxAge:     7,                     // 最多保存多少天
+			MaxBackups: 5,                     // 最多备份多少个
+			Compress:   true,                  // 压缩
+		}), zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level >= zapcore.DebugLevel
+		})),
+
 		zapcore.NewCore(zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
 			MessageKey:    "M",
 			LevelKey:      "",
